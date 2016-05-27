f1ea3728d56c55286f273dd8d88a1bdcb1e62f66
Patrick Cloke <clokep@patrick.cloke.us>
2016-05-27 17:05:07 -0400

    Add more tests.

--- tests/tests.py
+++ tests/tests.py
@@ -1,4 +1,4 @@
-from unittest import TestCase
+from unittest import skip, TestCase
 
 from django.template import Context
 from django.utils import six
@@ -11,21 +11,22 @@ class TestCases(TestCase):
         result = exception.message if six.PY2 else exception.args[0]
         self.assertEqual(expected, result)
 
-    def test_html1_block1(self):
+    def test_block(self):
+        """Test rendering an individual block."""
         result = render_block_to_string('test1.html', 'block1', Context({}))
         self.assertEqual(result, u'block1 from test1')
 
-    def test_html1_block2(self):
+        # No reason this shouldn't work, but just in case.
         result = render_block_to_string('test1.html', 'block2', Context({}))
         self.assertEqual(result, u'block2 from test1')
 
-    def test_html2_block1(self):
-        """This block is overridden in html1."""
+    def test_override(self):
+        """This block is overridden in test2."""
         result = render_block_to_string('test2.html', 'block1', Context({}))
         self.assertEqual(result, u'block1 from test2')
 
-    def test_html2_block2(self):
-        """This block is inherited from html1."""
+    def test_inherit(self):
+        """This block is inherited from test1."""
         result = render_block_to_string('test2.html', 'block2', Context({}))
         self.assertEqual(result, u'block2 from test1')
 
@@ -35,3 +36,20 @@ class TestCases(TestCase):
             render_block_to_string('test1.html', 'noblock')
         self.assertExceptionMessageEquals(exc.exception,
                                           "block with name 'noblock' does not exist")
+
+    def test_include(self):
+        """Ensure that an include tag in a block still works."""
+        result = render_block_to_string('test3.html', 'block1', Context({}))
+        self.assertEqual(result, u'included template')
+
+    @skip('Currently not supported.')
+    def test_block_include(self):
+        """Test rendering of a block which is included from another template."""
+        result = render_block_to_string('test4.html', 'block1', Context({}))
+        self.assertEqual(result, u'block1 from test1')
+
+    @skip('Currently not supported.')
+    def test_super(self):
+        """Test that block.super works."""
+        result = render_block_to_string('test3.html', 'block2', Context({}))
+        self.assertEqual(result, u'block2 from test3 - block2 from test1')
