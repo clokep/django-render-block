11eef8fd78cf0f2e63c603de7fce0b52ae07dee5
Patrick Cloke <clokep@patrick.cloke.us>
2016-05-27 22:41:10 -0400

    Initial support for Jinja2.

--- render_block/base.py
+++ render_block/base.py
@@ -2,6 +2,13 @@ from __future__ import absolute_import
 
 from django.template import loader
 from django.template.backends.django import Template as DjangoTemplate
+try:
+    from django.template.backends.jinja2 import Template as Jinja2Template
+except ImportError:
+    # Most likely Jinja2 isn't installed, in that case just create a class since
+    # we always want it to be false anyway.
+    class Jinja2Template(object):
+        pass
 
 from render_block.django import django_render_block
 from render_block.exceptions import BlockNotFound, UnsupportedEngine
@@ -30,6 +37,11 @@ def render_block_to_string(template_name, block_name, context=None):
     # The Django backend.
     if isinstance(t, DjangoTemplate):
         return django_render_block(t, block_name, context)
+
+    elif isinstance(t, Jinja2Template):
+        from render_block.jinja2 import jinja2_render_block
+        return jinja2_render_block(t, block_name, context)
+
     else:
         raise UnsupportedEngine(
             'Can only render blocks from the Django template backend.')
