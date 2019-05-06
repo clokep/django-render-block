ddff782ddc01c2b8af67c30c9eaee4cc5cf8f4a7
Patrick Cloke <clokep@patrick.cloke.us>
2019-05-06 17:24:35 -0400

    Fix broken build.

--- render_block/base.py
+++ render_block/base.py
@@ -11,7 +11,7 @@ except ImportError:
         pass
 
 from render_block.django import django_render_block
-from render_block.exceptions import UnsupportedEngine
+from render_block.exceptions import BlockNotFound, UnsupportedEngine
 
 
 def render_block_to_string(template_name, block_name, context=None):
