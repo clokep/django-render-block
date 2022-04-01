5057e7ca2c2df6cadc6100c680c3c70678750f67
Patrick Cloke <clokep@patrick.cloke.us>
2022-04-01 14:00:38 -0400

    Fix flake8 errors.

--- render_block/__init__.py
+++ render_block/__init__.py
@@ -1,2 +1,4 @@
 from render_block.base import render_block_to_string
 from render_block.exceptions import BlockNotFound, UnsupportedEngine
+
+__all__ = ["BlockNotFound", "UnsupportedEngine", "render_block_to_string"]
